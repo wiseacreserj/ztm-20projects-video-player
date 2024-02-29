8db248e14a93611aaeedeb5ab44053df3867eedb
Sergey Zadorozhniy <wiseacreserj@gmail.com>
2024-03-01 00:15:38 +0200

    done basic progress bar and complete display time update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Elements
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -10,11 +11,52 @@ const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
+const showPlayIcon = () => {
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+};
+
+const togglePlay = () => {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace("fa-play", "fa-pause");
+        playBtn.setAttribute("title", "Pause");
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+};
+
+//On video end, show play button icon
+video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
+
+//Calculate display time format
+
+const disaplayTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+};
+
+//Update progress bar as video plays
+
+const updateProgress = () => {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${disaplayTime(video.currentTime)} / `;
+    duration.textContent = `${disaplayTime(video.duration)}`;
+};
 
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
+
+//Event Listeners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
