b5cbbf2ab4921275dd59002da18796692f12d629
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-02 22:56:46 +0200

    done with volume controls and mute

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ video.addEventListener("ended", showPlayIcon);
 
 //Calculate display time format
 
-const disaplayTime = (time) => {
+const displayTime = (time) => {
     const minutes = Math.floor(time / 60);
     let seconds = Math.floor(time % 60);
     seconds = seconds > 9 ? seconds : `0${seconds}`;
@@ -45,8 +45,8 @@ const disaplayTime = (time) => {
 
 const updateProgress = () => {
     progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
-    currentTime.textContent = `${disaplayTime(video.currentTime)} / `;
-    duration.textContent = `${disaplayTime(video.duration)}`;
+    currentTime.textContent = `${displayTime(video.currentTime)} / `;
+    duration.textContent = `${displayTime(video.duration)}`;
 };
 
 //Click to seek within the video
@@ -57,7 +57,50 @@ const setProgress = (event) => {
     video.currentTime = newTime * video.duration;
 };
 
+let lastVolume = 1;
+
 // Volume Controls --------------------------- //
+const changeVolume = (event) => {
+    let volume = event.offsetX / volumeRange.offsetWidth;
+    //Rounding volume
+    if (volume < 0.1) {
+        volume = 0;
+    }
+    if (volume > 0.9) {
+        volume = 1;
+    }
+
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    //Change icon depending on volume
+    volumeIcon.className = "";
+    if (volume > 0.7) {
+        volumeIcon.classList.add("fas", "fa-volume-up");
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add("fas", "fa-volume-down");
+    } else if (volume === 0) {
+        volumeIcon.classList.add("fas", "fa-volume-off");
+    }
+    lastVolume = volume;
+};
+
+//Mute/Umute
+
+const toggleMute = () => {
+    volumeIcon.className = "";
+    if (video.volume) {
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add("fas", "fa-volume-mute");
+        volumeIcon.setAttribute("title", "Unmute");
+    } else {
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add("fas", "fa-volume-up");
+        volumeIcon.setAttribute("title", "Mute");
+    }
+};
 
 // Change Playback Speed -------------------- //
 
@@ -69,3 +112,5 @@ video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
+volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
