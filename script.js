3ad66e9d599838be04c9882750da7840b926b29a
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-02 23:37:35 +0200

    add fullscreen feature, project done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //Elements
+const player = document.querySelector(".player");
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -8,6 +9,7 @@ const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
+const speed = document.querySelector(".player-speed");
 const fullscreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
@@ -103,8 +105,51 @@ const toggleMute = () => {
 };
 
 // Change Playback Speed -------------------- //
+const changeSpeed = () => {
+    video.playbackRate = speed.value;
+};
+
+//Functions for fullscreen
+
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    video.classList.add("video-fullscreen");
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+    }
+    video.classList.remove("video-fullscreen");
+}
+
+let fullscreen = false;
 
 // Fullscreen ------------------------------- //
+
+const toggleFullscreen = () => {
+    if (!fullscreen) {
+        openFullscreen(player);
+    } else {
+        closeFullscreen();
+    }
+    fullscreen = !fullscreen;
+};
 
 //Event Listeners
 playBtn.addEventListener("click", togglePlay);
@@ -114,3 +159,5 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
+fullscreenBtn.addEventListener("click", toggleFullscreen);
